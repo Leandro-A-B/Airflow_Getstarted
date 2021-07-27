b584ca723dd67e2c3fddf34fb5c1f1f3a29d5174
Leandro-A-B <leandro.bueno@df.ufscar.br>
2021-07-27 13:41:54 -0300

    mudança nas dags

--- dags/leandro.py
+++ dags/leandro.py
@@ -2,21 +2,30 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from datetime import datetime, timedelta
 import pandas as pd
+import pymongo
 
 
-def _extrai_dados():
+def _extrai_dados_mongo():
+    return print("olá leandro do mongo")
+
+def _extrai_dados_ibge():
+    return print("olá leandro do ibge")
+
     return
 
 
 with DAG ("minha_dagzinha", start_date=datetime(2021,1,1),
             schedule_interval="@daily", catchup=False) as dag:
         
-        atividade1 = PythonOperator(
+        extrai_dados_mongo = PythonOperator(
             task_id="extrai_dados_mongo"
             python_callabable=_extrai_dados
         )
 
-        atividade2 = PythonOperator(
-            task_id="extrai_dados_"
+        extrai_dados_ibge = PythonOperator(
+            task_id="extrai_dados_ibge"
             python_callabable=_extrai_dados
         )
+
+
+extrai_dados_ibge >> extrai_dados_mongo
